9ad08e29bd759476839513bf3d8f5a192c0ba25e
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-06-14 14:46:06 -0600

    Add exprience section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -16,10 +16,11 @@ const ExperienceCard = ({ experience }) => {
   return (
     <VerticalTimelineElement
       contentStyle={{
-        background: "#1d1836",
-        color: "#fff",
+        background: "#000",
+        color: "#000",
+        border: "5px double #ffffff",
       }}
-      contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+      contentArrowStyle={{ borderRight: "7px solid #000" }}
       date={experience.date}
       iconStyle={{ background: experience.iconBg }}
       icon={
@@ -35,7 +36,7 @@ const ExperienceCard = ({ experience }) => {
       <div>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
         <p
-          className='text-secondary text-[16px] font-semibold'
+          className='text-[16px] font-semibold'
           style={{ margin: 0 }}
         >
           {experience.company_name}
@@ -46,7 +47,7 @@ const ExperienceCard = ({ experience }) => {
         {experience.points.map((point, index) => (
           <li
             key={`experience-point-${index}`}
-            className='text-white-100 text-[14px] pl-1 tracking-wider'
+            className='text-white text-[14px] pl-1 tracking-wider'
           >
             {point}
           </li>
@@ -60,7 +61,7 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} text-center`}>
+        <p className="sm:text-[18px] text-[14px] text-black uppercase tracking-wider text-center">
           What I have done so far
         </p>
         <h2 className={`${styles.sectionHeadText} text-center`}>
