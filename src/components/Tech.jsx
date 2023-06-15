2979921f15c4d8255507be8f90437c57a0861e0b
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-06-15 10:54:27 -0600

    Modify tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,13 +6,17 @@ import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
+    <div>
+    <h2 className="text-yellow-400 font-black text-center md:text-6xl sm:text-5xl xs:text-4xl text-3xl">Skills</h2>
+    <h3 className="sm:text-18 text-14 text-white uppercase tracking-wider text-center">TECHNOLOGIES</h3>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
       {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
+        <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
+  </div>
   );
 };
 
