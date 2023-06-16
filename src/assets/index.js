9aa8177ba336b4e5f9ae66c5b578dcb3d9028e88
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-06-16 14:23:48 -0600

    Import resource live

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,7 @@ import creator from "./creator.png";
 import mobile from "./mobile.png";
 import web from "./web.png";
 import github from "./github.png";
+import live from "./live.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
@@ -57,6 +58,7 @@ export {
   pokeapi,
   metric,
   ruby,
+  live,
   rubyRails,
   fusion1,
   book,
