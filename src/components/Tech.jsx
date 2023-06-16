11617220bc37c02dec9253826e2fc9f78d403a67
Pierre Barba <99061965+PierreBarba@users.noreply.github.com>
2023-06-16 14:25:16 -0600

    Modify style tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-
-// import { BallCanvas } from "./canvas";
+import { technologies } from "../constants/index";
 import { SectionWrapper } from "../hoc";
-import { technologies } from "../constants";
+import { motion } from "framer-motion";
+
 
 const Tech = () => {
   return (
     <div>
-    <h2 className="text-yellow-400 font-black text-center md:text-6xl sm:text-5xl xs:text-4xl text-3xl">Skills</h2>
-    <h3 className="sm:text-18 text-14 text-white uppercase tracking-wider text-center">TECHNOLOGIES</h3>
-    <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28" key={technology.name}>
-          {
-            <div></div>
-          }
+  <h2 className="text-yellow-400 font-black text-center md:text-6xl sm:text-5xl xs:text-4xl text-3xl">Skills</h2>
+  <h3 className="text-sm text-white uppercase tracking-wider text-center my-4">TECHNOLOGIES</h3>
+  <div className="flex flex-row flex-wrap justify-center gap-10 ">
+    {technologies.map((technology) => (
+      <div
+      className="w-28 h-34 hover:border-solid border-double border-4 border-black bg-white rounded-[20px] hover:border-double-black"
+      key={technology.name}
+    >
+        <div>
+          <img src={technology.icon} alt={technology.name} />
         </div>
-      ))}
-    </div>
+      </div>
+    ))}
   </div>
+</div>
+
   );
 };
 
 export default SectionWrapper(Tech, "");
-
-// esto va dentro del tecnologies.map -  /* <BallCanvas icon={technology.icon} /> */
